f263ff0d7ce1cfaa617e5b2d53729ff7fcabf829
Jordi Mas <jmas@softcatala.org>
2023-10-05 22:32:37 +0200

    Fix

--- pair.py
+++ pair.py
@@ -2,6 +2,31 @@ import sys
 import datetime
 import zlib
 
+def _is_sentence_len_good(src, trg):
+    src = src.strip()
+    trg = trg.strip()
+    lsrc = len(src)
+    ltrg = len(trg)
+
+    if lsrc == 0 or ltrg == 0:
+        return False
+
+    MIN_CHARS = 50
+    if max(lsrc, ltrg) > MIN_CHARS:
+        size_diff_percentage = 50
+        if size_diff_percentage > 0:
+            if lsrc < ltrg:
+               tmp = lsrc
+               lsrc = ltrg
+               ltrg = tmp
+
+            diff = (lsrc - ltrg) / lsrc * 100
+            if diff > size_diff_percentage:
+                return False
+
+    return True
+
+
 with open("data.1st/en.txt", "r") as f_src, open("data.1st/ca.txt", "r") as f_tgt, open("pair-debug.txt", "w") as f_output_debug:
     srcs = {}
     for line in f_src.readlines():
@@ -21,10 +46,17 @@ with open("data.1st/en.txt", "r") as f_src, open("data.1st/ca.txt", "r") as f_tg
     with open("en.txt", "w") as f_src, open("ca.txt", "w") as f_tgt:
         total = 0
         written = 0
+        diff = 0
         for _id, src_str in srcs.items():
             tgt_src = tgts.get(_id)
             if not tgt_src:
                 continue
+                
+            if not _is_sentence_len_good(src_str, tgt_str):
+                print(f"{src_src}")
+                print(f"{tgt_src}\n")   
+                diff += 1
+                continue                               
 
             total += 1
             crc = zlib.crc32(bytes(src_str, "utf-8"))
@@ -40,4 +72,4 @@ with open("data.1st/en.txt", "r") as f_src, open("data.1st/ca.txt", "r") as f_tg
             f_output_debug.write(f"{src_str}\n")
             f_output_debug.write(f"{tgt_src}\n")
 
-        print(f"Total: {total}, written: {written}")
+        print(f"Total: {total}, written: {written}, diff discarted {diff}")
