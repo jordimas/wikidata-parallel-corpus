9c76275444de95dfafcabc3af7bde80d95ef8e5d
Jordi Mas <jmas@softcatala.org>
2023-10-05 22:04:16 +0200

    Fixes

--- pair.py
+++ pair.py
@@ -1,27 +1,23 @@
-import ijson
-import json
 import sys
 import datetime
-import zlib
-import collections
-import operator
 
 with open("data.1st/en.txt", "r") as f_src, open("data.1st/ca.txt", "r") as f_tgt:
     srcs = {}
-    for line in f_src.readlines()[0:500000]:
+    for line in f_src.readlines():
         components = line.split("\t")
         src_id = components[0]
         src_str = components[1]
         srcs[src_id] = src_str
 
     tgts = {}
-    for line in f_tgt.readlines()[0:500000]:
+    for line in f_tgt.readlines():
         components = line.split("\t")
         tgt_id = components[0]
         tgt_str = components[1]
         tgts[src_id] = tgt_str
 
     with open("en.txt", "w") as f_src, open("ca.txt", "w") as f_tgt:
+        written = 0
         for _id, src_str in srcs.items():
             tgt_src = tgts.get(_id)
             if not tgt_src:
@@ -29,3 +25,5 @@ with open("data.1st/en.txt", "r") as f_src, open("data.1st/ca.txt", "r") as f_tg
                 
             f_src.write(f"{src_str}\n")
             f_tgt.write(f"{tgt_src}\n")
+            written += 1
+        print(f"Written: {written}")            
