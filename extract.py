92766c10404f16ad6faa3ffdda479d55a2e63022
Jordi Mas <jmas@softcatala.org>
2023-10-06 09:26:42 +0200

    Fix

--- extract.py
+++ extract.py
@@ -1,33 +1,35 @@
 import ijson
-import json
 import sys
 import datetime
-import zlib
-import collections
 import operator
 import os
+
 
 def ensure_dir(directory):
     isExist = os.path.exists(directory)
     if not isExist:
-       os.makedirs(directory)
+        os.makedirs(directory)
+
+
+def inc_string_counter_for_lang(languages, language):
+    counter = languages.get(language, 0) + 1
+    languages[language] = counter
+
 
 def extract(directory):
-    items = ijson.items(sys.stdin, "item", use_float=True)
-    cnt = 0
-
     start_time = datetime.datetime.now()
 
+    items = ijson.items(sys.stdin, "item", use_float=True)
     languages = {}
     files = {}
+    parts = {}
     processed = 0
+    MAX_LINES = 500
     for item in items:
         if processed % 1000000 == 0:
             print(f"item processed: {processed}")
 
         processed += 1
-
-        new_item = {}
 
         _id = item.get("id")
         if not _id:
@@ -36,15 +38,30 @@ def extract(directory):
         descriptions = item.get("descriptions")
         for language in descriptions.keys():
             description = descriptions[language].get("value")
-            if len(description) > 0:
-                counter = languages.get(language, 0) + 1
-                languages[language] = counter
-                file_h = files.get(language)
-                if not file_h:
-                    file_h = open(f"{directory}/{language}.txt", "w")
-                    files[language] = file_h
+            if len(description) == 0:
+                continue
 
-                file_h.write(f"{_id}\t{description}\n")
+            inc_string_counter_for_lang(languages, language)
+
+            file_h = files.get(language)
+            part, lines = parts.get(language, (1, 0))
+            lines += 1
+
+            file_h = files.get(language)
+
+            if not file_h:
+                file_h = open(f"{directory}/{language}-{part}.txt", "w")
+                files[language] = file_h
+            elif lines > MAX_LINES:
+                lines = 0
+                part += 1
+
+                file_h.close()
+                file_h = open(f"{directory}/{language}-{part}.txt", "w")
+                files[language] = file_h
+
+            file_h.write(f"{_id}\t{description}\n")
+            parts[language] = (part, lines)
 
         if processed > 1000:
             break
@@ -58,11 +75,12 @@ def extract(directory):
     s = "Time used: {0}".format(datetime.datetime.now() - start_time)
     print(s)
 
+
 def main():
     directory = "data/"
     ensure_dir(directory)
     extract(directory)
 
+
 if __name__ == "__main__":
     main()
-    
